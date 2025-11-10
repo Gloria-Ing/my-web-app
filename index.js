8e722ae306ebdd29adb5431e4810becd61d34ca3
Gloria-Ing <ingloria446@gmail.com>
2025-11-10 07:26:09 -0800

    Add /about route to web application

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const http = require ('http');
-const server = http.createServer ((req,res) => {
-res.writeHead(200, {'Content-Type': 'text/plain' });
-res.end('Hello Gloria');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, Gloria! Your Node.js server is running 💖');
+  } else if (req.url === '/about') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('About Page');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Page not found');
+  }
 });
 
-const PORT=3000;
+const PORT = 3000;
 server.listen(PORT, () => {
-console.log(`Server running at http:// localhost:${PORT}/`);
+  console.log(`Server running at http://localhost:${PORT}/`);
 });
